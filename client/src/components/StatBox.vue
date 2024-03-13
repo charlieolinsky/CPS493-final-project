07a2e393fe6b88671abf951ded95f17b8ece23f0
charlieolinsky <charlesolinsky@gmail.com>
2024-03-13 02:03:22 -0400

    Styled Landing Page

---
+++ client/src/components/StatBox.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  distance: number;
+  duration: number;
+  avgPace: number;
+  calories: number;
+}>();
+</script>
+
+<template>
+  <div class="box has-text-success summary">
+    <h2 class="title">{{ title }}</h2>
+    <div class="column is-multiline">
+      <div class="column is-half">
+        <h3 class="value">{{ distance }}</h3>
+        <caption class="caption">
+          Distance
+        </caption>
+      </div>
+      <div class="column is-half">
+        <h3 class="value">{{ duration }}</h3>
+        <caption class="caption">
+          Duration
+        </caption>
+      </div>
+      <div class="column is-half">
+        <h3 class="value">{{ avgPace }}</h3>
+        <caption class="caption">
+          Avg Pace
+        </caption>
+      </div>
+      <div class="column is-half">
+        <h3 class="value">{{ calories }}</h3>
+        <caption class="caption">
+          Calories
+        </caption>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { watchEffect } from "vue";
 import { getUserStore } from "../global/users";
+import StatBox from "@/components/StatBox.vue";
 
 //DEBUG --START
 const userStore = getUserStore();
@@ -11,8 +12,34 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Landing Page!</h1>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-one-quarter"></div>
+      <div class="column">
+        <StatBox
+          :title="'Today'"
+          :distance="0"
+          :duration="0"
+          :avgPace="0"
+          :calories="0"
+        />
+        <StatBox
+          :title="'This Week'"
+          :distance="0"
+          :duration="0"
+          :avgPace="0"
+          :calories="0"
+        />
+        <StatBox
+          :title="'All Time'"
+          :distance="3.8"
+          :duration="2.5"
+          :avgPace="1.4"
+          :calories="1127.5"
+        />
+      </div>
+      <div class="column is-one-quarter"></div>
+    </div>
   </div>
 </template>
 
